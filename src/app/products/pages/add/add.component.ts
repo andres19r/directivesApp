2af8c71aeaaefc4da7fe267c2827b93a83723451
Andres Rivero <andres19rivero@gmail.com>
2022-09-05 21:24:35 -0400

    set reactive form to add product form

--- src/app/products/pages/add/add.component.ts
+++ src/app/products/pages/add/add.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add',
   templateUrl: './add.component.html',
-  styleUrls: ['./add.component.css']
+  styleUrls: ['./add.component.css'],
 })
 export class AddComponent implements OnInit {
+  myForm: FormGroup = this.fb.group({
+    name: ['', Validators.required],
+  });
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  hasError(field: string): boolean {
+    return this.myForm.get('name')?.invalid || false;
   }
-
 }
