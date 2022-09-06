d1beab0fd19fc3518175f32b7162d59d4bb206f4
Andres Rivero <andres19rivero@gmail.com>
2022-09-05 21:37:57 -0400

    create error-msg directive

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { ErrorMsgDirective } from './directives/error-msg.directive';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [ErrorMsgDirective],
+  imports: [CommonModule],
+  exports: [ErrorMsgDirective],
 })
-export class SharedModule { }
+export class SharedModule {}
